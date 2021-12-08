809bf562de679c64c56664e915764cd3009a7385
Mdavichick <78510482+Mdavichick@users.noreply.github.com>
2021-12-07 17:05:59 -0800

    Fixed CSS issues

--- src/loginPage.js
+++ src/loginPage.js
@@ -9,17 +9,17 @@ const Login =() =>{
         <body class="html not-logged-in bg">
         <div id="center">
             <img class="img-responsive" src={logo} alt ={logo}/>
-            <h1 class="text-center margin-15 padding-15"> uLearn Login</h1>
+            <h1 className ="mike" class="text-center margin-15 padding-15"> uLearn Login</h1>
             <form title="SF State Global Login">
 
             <div class="form-group">
-                                    <label for="username" >SF State ID or Email</label>
+                                    <label className ="loginLabel" for="username" >SF State ID or Email</label>
                                     <input class="form-control" id="username" name="j_username" type="text" tabindex="1"
                                                value="" autofocus></input>
             </div>
 
             <div class="form-group">
-                                    <label for="password" class="control-label" >SF State Password</label>
+                                    <label className ="loginLabel" for="password" class="control-label" >SF State Password</label>
                                         <input class="form-control" id="password" name="j_password" tabindex="2" type="password" value=""></input>
             </div>
 
@@ -30,11 +30,11 @@ const Login =() =>{
                                   </div>
             </form>
 
-            <p class="text-center padding-top-20">
+            <p className="test"class="text-center padding-top-20">
                   <a class="login" href="https://its.sfsu.edu/helpsignin">Get help signing in</a>
                 </p>
 
-                <p class="text-center padding-top-20">
+                <p className="test" class="text-center padding-top-20">
 				<a className= "tfa" class="tfa" href="https://2fa.sfsu.edu">Not enrolled in 2FA? Enroll now</a>
                         </p>
         </div>
