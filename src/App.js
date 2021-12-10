5abc3c27caf666f0bacee3ed3bc1d5628bc35a96
LukeKnezCK <lchangknezevich@mail.sfsu.edu>
2021-12-09 22:54:57 -0800

    github pages routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import StudentCalendar from './StudentCalendar';
 import StudentTaskList from './StudentTaskList';
 import Home from './Home';
